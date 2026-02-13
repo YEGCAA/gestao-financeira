3667af437ef2e525403d0c1a7ce38a6ec53b9463
Even Digital <contato@evendigital.com.br>
2026-02-13 14:42:34 -0300

    fix: Adicionado import do index.css que estava causando tela branca

--- index.tsx
+++ index.tsx
@@ -1,6 +1,7 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './index.css';
 import AuthWrapper from './AuthWrapper';
 
 const rootElement = document.getElementById('root');
